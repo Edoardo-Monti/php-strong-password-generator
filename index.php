989abbc25fa8bf6bd598659a72513c9f5141f436
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-22 15:56:55 +0200

    sposto la funzione in un file a parte

--- index.php
+++ index.php
@@ -26,29 +26,7 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
 $sceltaLength = $_GET['passwordLength'];
 var_dump($sceltaLength);
 
-
-function generaPasswordCasuale($lunghezza) {
-    //creiamo una stringa con tutti i simboli che vogliamo utilizzare
-    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%*-_';
-    //prendiamo la lunghezza della stringa
-    $lunghezzaCaratteri = strlen($caratteri);
-    //inizializiamo una variabile vuota dove ci pusheremo gli elementi singoli
-    $password = '';
-
-    //facciamo un ciclo fino alla lunghezza che inseriamo nell' input
-    for ($i = 0; $i < $lunghezza; $i++) {
-        //generare un numero casuale compreso tra 0 e $lunghezzaCaratteri e utilizza questo numero per selezionare un carattere casuale dalla stringa $caratteri 
-        $carattereCasuale = $caratteri[rand(0, $lunghezzaCaratteri) - 1];
-        //aggingiamo ogni carattere trovato alla variabile $password
-        $password .= $carattereCasuale;
-    }
-
-    return $password;
-}
-generaPasswordCasuale($sceltaLength);
-var_dump(generaPasswordCasuale($sceltaLength));
-
-
+include __DIR__ . '/include/partials/function.php';
 
 ?>
 
